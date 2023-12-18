c33f59c134d983983af1f8952c02fbf1bbb08e75
the-sides <jacob.r.sides@gmail.com>
2023-12-17 23:35:53 -0500

    Fetch only when necessary and clear

--- skeleton-app/src/routes/movies/+page.svelte
+++ skeleton-app/src/routes/movies/+page.svelte
@@ -2,20 +2,23 @@
     import Movie from "./Movie.svelte";
 
     export let data;
-    let query = '';
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${data.token}&query=`
+    let query, movies;
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${data.token}&query=`;
 
-    $: movies = fetch(url + query).then(rs => rs.json())
+    $: if (query) movies = fetch(url + query).then((rs) => rs.json());
+    $: if (query === '') movies = null;
 </script>
 
 <section>
-    <input type="text" bind:value={query}>
+    <input type="text" bind:value={query} />
     {#await movies}
-        Loading...        
-    {:then data} 
-        {#each data.results as movie}
-            <Movie {...movie}/>
-        {/each}
+        Loading...
+    {:then data }
+        {#if data}
+            {#each data.results as movie}
+                <Movie {...movie} />
+            {/each}
+        {/if}
     {/await}
 </section>
 
