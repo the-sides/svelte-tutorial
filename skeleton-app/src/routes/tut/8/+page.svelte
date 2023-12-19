408f48fcbfd31932e4f267be11551bb32aaac0d0
the-sides <jacob.r.sides@gmail.com>
2023-12-19 00:04:41 -0500

    Finish Tutorial

---
+++ skeleton-app/src/routes/tut/8/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { Confetti } from "svelte-confetti"
+</script>
+<section>
+    <h2>Congrats! You now know <sub>(or at least can recognize)</sub> Svelte!</h2>
+    <h2>Intro to SvelteKit</h2>
+    <Confetti x={[-1, 5]} y={[-1, 5]} />
+</section>
