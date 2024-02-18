44a328ddbfa1206acfeea2b90613886df260ce6b
the-sides <jacob.r.sides@gmail.com>
2024-02-17 19:03:38 -0500

    Use different token

--- skeleton-app/src/routes/search/+server.js
+++ skeleton-app/src/routes/search/+server.js
@@ -1,11 +1,11 @@
 import { json } from '@sveltejs/kit';
-import { env } from '$env/dynamic/public'
+import { SECRET_READ_TOKEN } from '$env/static/private'
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET({ url }) {
     // https://joyofcode.xyz/sveltekit-environment-variables
     const query = url.searchParams.get('q');
-    const movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${env.PUBLIC_READ_TOKEN}&query=${query}`;
+    const movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${SECRET_READ_TOKEN}&query=${query}`;
     const res = await fetch(movieUrl).then(d => d.json());
 
     return json(res);
